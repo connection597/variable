6abb3a08b6ecb3de1edd276a5447b9258c044999
connection579 <wlswn4118@naver.com>
2020-10-29 02:25:06 +0900

    정수형,실수형 연습

---
+++ src/variable/VariableEx1.java
@@ -0,0 +1,28 @@
+package variable;
+
+public class VariableEx1 {
+
+	public static void main(String[] args) {
+		int a ; // 정수형 int에 a 선언
+		a = 10; // a의 값을 10
+		System.out.println("a = " + a);
+		
+		a = 20;
+		System.out.println("a = " + a);
+		System.out.println("a + a= " + (a+a));
+		
+		int b = 30; 
+		System.out.println("b = " + b);
+		
+		float f; //실수형 float선언
+		f= 0.12345678945689f; //실수형을 쓸때 식별자로 f를 써준다
+		System.out.println("f = " + f);
+		
+		double d;
+		d = 0.12345678912345689456789;
+		System.out.println("d = " + d);
+		
+
+	}
+
+}
